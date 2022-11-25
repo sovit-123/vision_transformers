9100088adf11ee309ab01bf8f8a3d349e9406c57
sovit-123 <sovitrath5@gmail.com>
2022-11-25 22:55:07 +0530

    Create pypi package - pip install vision-transformers

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'vision_transformers',
+    packages = find_packages(),
+    version = '0.0.1',
+    license = 'MIT',
+    description = 'Vision Transformers (ViT)',
+    long_description_content_type = 'text/markdown',
+    author = 'Sovit Rath',
+    author_email = 'sovitrath5@gmail.com',
+    url = 'https://github.com/sovit-123/vision_transformers',
+    keywords = [
+        'vision',
+        'neural attention',
+        'deep learning'
+    ],
+    install_requires = [
+        'torch>=1.10',
+        'torchvision'
+    ],
+    setup_requires = [],
+    tests_require = [],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
