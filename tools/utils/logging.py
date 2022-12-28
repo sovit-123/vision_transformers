97c5ef3f604511e9a9534971de75250448f2e1e9
sovit-123 <sovitrath5@gmail.com>
2022-12-28 07:36:03 +0530

    Added local logging

---
+++ tools/utils/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def set_log(log_dir):
+    logging.basicConfig(
+        # level=logging.DEBUG,
+        format='%(message)s',
+        # datefmt='%a, %d %b %Y %H:%M:%S',
+        filename=f"{log_dir}/train.log",
+        filemode='w'
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # add the handler to the root logger
+    logging.getLogger().addHandler(console)
+
+def log(content, *args):
+    for arg in args:
+        content += str(arg)
+    logger.info(content)
