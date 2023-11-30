a382a95ef0c6493b63c85233b65fd16c6da93941
sovit-123 <sovitrath5@gmail.com>
2023-11-30 16:13:28 +0530

    Corrected attention map saving for custom models

--- tools/utils/detection/detr/viz_attention.py
+++ tools/utils/detection/detr/viz_attention.py
@@ -16,18 +16,33 @@ def visualize_attention(
     # use lists to store the outputs via up-values
     conv_features, enc_attn_weights, dec_attn_weights = [], [], []
 
-    hooks = [
-        model.backbone[-2].register_forward_hook(
-            lambda self, input, output: conv_features.append(output)
-        ),
-        model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
-            lambda self, input, output: enc_attn_weights.append(output[1])
-        ),
-        model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
-            lambda self, input, output: dec_attn_weights.append(output[1])
-        ),
-    ]
-
+    # For COCO pretrained model.
+    try:
+        hooks = [
+            model.backbone[-2].register_forward_hook(
+                lambda self, input, output: conv_features.append(output)
+            ),
+            model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
+                lambda self, input, output: enc_attn_weights.append(output[1])
+            ),
+            model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
+                lambda self, input, output: dec_attn_weights.append(output[1])
+            ),
+        ]
+    # For custom model which is enclosed within `model` block.
+    except:
+        hooks = [
+            model.model.backbone[-2].register_forward_hook(
+                lambda self, input, output: conv_features.append(output)
+            ),
+            model.model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
+                lambda self, input, output: enc_attn_weights.append(output[1])
+            ),
+            model.model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
+                lambda self, input, output: dec_attn_weights.append(output[1])
+            ),
+        ]
+    
     # propagate through the model
     outputs = model(inputs.to(device))
 
